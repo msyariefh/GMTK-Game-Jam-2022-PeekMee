71142d28b45c5e98fa661b659719690b35fe1d94
HiDE <msyariefhidayatulloh14@gmail.com>
2022-07-16 12:27:49 +0700

    Tambahin gacha

--- Assets/Scripts/UnitStats.cs
+++ Assets/Scripts/UnitStats.cs
@@ -5,9 +5,42 @@ using UnityEngine;
 public class UnitStats : MonoBehaviour
 {
     public int maxHP;
-    public int currentHP;
-    public int basicAttack;
-    public int specialAttackPercentage;
-    public int regenHP;
+    private int currentHP;
+    [SerializeField] private int basicAttack;
+    [SerializeField] private int specialAttackPercentage;
+    [SerializeField] private int regenHP;
 
+
+    public void Attack (UnitStats other, int multiplication)
+    {
+        if (other.currentHP - basicAttack*multiplication <= 0)
+        {
+            // WON!
+            return;
+        }
+        other.Attacked(basicAttack * multiplication);
+    }
+
+    public void Attacked(int attackPoint)
+    {
+        currentHP -= attackPoint;
+    }
+
+    public void Heal(int percentage)
+    {
+        int addHealth = Mathf.FloorToInt(percentage / 100 * currentHP);
+        if (currentHP + addHealth >= maxHP)
+        {
+            currentHP = maxHP;
+        }
+        else
+        {
+            currentHP += addHealth;
+        }
+    }
+
+    public void SpecialAttack(UnitStats other)
+    {
+        other.Attacked(Mathf.FloorToInt(other.maxHP * specialAttackPercentage / 100));
+    }
 }
